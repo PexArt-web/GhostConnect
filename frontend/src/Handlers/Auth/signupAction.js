c48a30b078a3f7116a7b248763e5807a27f19423
PexArt-web <pexart74@gmail.com>
2024-11-07 04:59:06 +0100

    GhostConnect

--- frontend/src/Handlers/Auth/signupAction.js
+++ frontend/src/Handlers/Auth/signupAction.js
@@ -1,5 +1,5 @@
 import { signupService } from "../../Utils/Auth/signupService";
-import { signUpSchema} from "../Schemas/signUpSchema";
+import { signUpSchema } from "../Schemas/signUpSchema";
 
 export const signupAction = async ({ request }) => {
   const formData = await request.formData();
@@ -8,12 +8,16 @@ export const signupAction = async ({ request }) => {
   const password = formData.get("password");
   try {
     signUpSchema.parse({ username, email, password });
-    if(!username || !email || !password){
+    if (!username || !email || !password) {
       throw Error("All fields are required");
     }
     const data = await signupService(username, email, password);
     return { user: data };
   } catch (error) {
+    error.message = "TypeError: Failed to fetch"
+      ? (error.message =
+          "Network error: Unable to connect. Please check your internet connection and try again.")
+      : error.message;
     return { error: error.message };
   }
 };
--- frontend/src/Handlers/Schemas/loginSchema.js
+++ frontend/src/Handlers/Schemas/loginSchema.js
@@ -1,9 +1,8 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  email: z.string().email("Invalid email address zod").required(),
+  email: z.string().email("Invalid email address zod"),
   password: z
     .string()
-    .min(6, "Password must be at least 6 characters")
-    .required(),
+    .min(6, "Password must be at least 6 characters"),
 });
